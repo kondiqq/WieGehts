497699ea2b035181f589df5f483e1cc54eca16bb
Konrad Kowalczyk <konrad.kowalczyk@capgemini.com>
2023-08-16 09:49:44 +0000

    Implemented smartable, changed data model to oDataV2, and cut _onPatternMatched to smaller foo

--- app/project1/webapp/controller/View1.controller.js
+++ app/project1/webapp/controller/View1.controller.js
@@ -17,17 +17,33 @@ sap.ui.define([
                 .attachPatternMatched(this._onPatternMatched, this);
             },
 
-            _onPatternMatched : function(oEvent){
-                debugger;
-                let oPropertiesModel = this.getView().getModel('tableViewModel');
+            _onPatternMatched : function(){
+                let oPropertiesModel =  this.getOwnerComponent().getModel('tableViewModel');
                 let oModel = this.getView().getModel();
+                this.getTheOldest(oModel, oPropertiesModel);
+                this.getTheOldest(oModel, oPropertiesModel);
+                this.getNoBook(oModel, oPropertiesModel);
+                this.getNoAuthor(oModel, oPropertiesModel);
+                console.log(123);
+            },
+
+            convert2GalacticCredits: function(sCurrency, iValue) {
+                let oModel = this.getView().getModel();
+                oModel.callFunction("", {
+                    method: "GET",
+                    urlParameters: {},
+                    success: function(oData) {},
+                    error: function(err) {}
+                });
+            },
+
+            getTheOldest: function(oModel, oSavedModel) {
                 oModel.callFunction("/getTheOldestAuthor", {
                     method: "GET",
                     success: function(oData) {
-                        const oOldestAuthor = JSON.parse(oData);
+                        const oOldestAuthor = oData.getTheOldestAuthorta;
                         console.log('OK', oData);
-                        oPropertiesModel.setProperty("/NoBook", oOldestAuthor);
-                        MessageBox.info(`That's work`);
+                        oSavedModel.setProperty("/Authors/OldestAuthor", oOldestAuthor);
                     },
                     error: function(err){
                         MessageBox.error(`It's something wrong`);
@@ -37,8 +53,52 @@ sap.ui.define([
                 });
             },
 
-            extendLibrary : function(){
+            getTheYounger: function(oModel, oSavedModel) {
+                oModel.callFunction("/getTheYoungestAuthor", {
+                    method: "GET",
+                    success: function(oData) {
+                        const oYoungestAuthor = oData.getTheYoungestAuthor;
+                        console.log('OK', oData);
+                        oSavedModel.setProperty("/Authors/YoungestAuthor", oYoungestAuthor);
+                    },
+                    error: function(err){
+                        MessageBox.error(`It's something wrong`);
+                        console.log('No OK', err);
+                        console.error(err);
+                    }
+                });
+            },
 
+            getNoBook: function(oModel, oSavedModel) {
+                oModel.callFunction("/getNoBook", {
+                    method: "GET",
+                    success: function(oData) {
+                        const iNoBook = oData.getNoBook;
+                        console.log('OK', oData);
+                        oSavedModel.setProperty("/Numbers/NoBook", iNoBook);
+                    },
+                    error: function(err){
+                        MessageBox.error(`It's something wrong`);
+                        console.log('No OK', err);
+                        console.error(err);
+                    }
+                });
+            },
+
+            getNoAuthor: function(oModel, oSavedModel) {
+                oModel.callFunction("/getNoAuthor", {
+                    method: "GET",
+                    success: function(oData) {
+                        const iNoAuthor = oData.getNoAuthor;
+                        console.log('OK', oData);
+                        oSavedModel.setProperty("/Numbers/NoAuthor", iNoAuthor);
+                    },
+                    error: function(err){
+                        MessageBox.error(`It's something wrong`);
+                        console.log('No OK', err);
+                        console.error(err);
+                    }
+                });
             },
         });
     });
